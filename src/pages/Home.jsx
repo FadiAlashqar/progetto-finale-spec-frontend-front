7c0be7c18ddd0557a52339643de7ab63b2a8e94f
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-09-18 18:43:48 +0200

    [UPD] folder setup & global context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <h1>HOME</h1>
+                    <h1><strong>HOME</strong></h1>
                 </div>
             </div>
         </div>
