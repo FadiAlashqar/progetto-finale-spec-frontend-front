bd4dd13b486640a388176c715124f728f6c0a680
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-09-25 17:58:25 +0200

    [UPD] favourite sidebar

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,8 +1,13 @@
 import React, { children, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import UseDevices from '../hooks/UseDevices'
+import { useContext } from 'react'
+import { GlobalContext } from '../contexts/GlobalContext'
 
 const DefaultLayout = ({ children }) => {
+
+    const { favDevice, setFavDevice } = useContext(GlobalContext)
+    console.log(favDevice)
 
     return (
         <>
@@ -12,14 +17,34 @@ const DefaultLayout = ({ children }) => {
                         <h1>Device Shop</h1>
                     </div>
                     <div className="col-8 d-flex justify-content-evenly">
-                        <div><NavLink to='/'>Home</NavLink></div>
-                        <div><NavLink to='/compare'>Compare</NavLink></div>
-                        <div><NavLink to='/favourites'>Favourites</NavLink></div>
+                        <div className=''><NavLink to='/'>Home</NavLink></div>
+                        <div className=''><NavLink to='/compare'>Compare</NavLink></div>
+                        <div className=''><NavLink to='/favourites'>Favourites</NavLink></div>
                     </div>
                 </div>
             </header>
-            <main>
-                {children}
+            <main className='d-flex'>
+                <div className="content flex-grow-1">
+                    {children}
+                </div>
+                <aside className="side-bar">
+                    <div className="fav-section">
+                        <div className="row">
+                            <div className="col-12 d-flex justify-content-center">
+                                <h3><i className="fa-solid fa-heart"></i></h3>
+                            </div>
+                            <div className="col-12">
+                                {
+                                    favDevice.map((f) => {
+                                        return <div key={f.id}>
+                                            <img className='img-fluid' src={f.img} />
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </aside>
             </main>
             <footer>
 
